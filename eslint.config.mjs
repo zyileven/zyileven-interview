03c5452073893a791d8afce92acb3f5d38985641
zyileven <zyileven@gmail.com>
2025-07-22 16:40:28 +0800

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     "rules": {
-      "@typescript-eslint/no-explicit-any": "warn"
+      '@typescript-eslint/no-explicit-any': 'off',
     }
   }
 ];
